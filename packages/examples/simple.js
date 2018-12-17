ea47301b7855a569d3c97aaf15a8bf0bf1450f5d
Holger Will <h.will@klimapartner.de>
2018-12-17 13:52:03 +0100

    final commit

--- packages/examples/simple.js
+++ packages/examples/simple.js
@@ -1,7 +1,7 @@
 /*eslint-disable*/
 import { SerialportSender } from '@enocean-js/serialport-sender'
 import { ESP3Parser } from '@enocean-js/serialport-parser'
-import { CommonCommand } from '@enocean-js/esp3-packets'
+import { CommonCommand } from '@enocean-js/common-command'
 import { pretty } from '@enocean-js/pretty-printer'
 
 const SerialPort = require('serialport')
@@ -13,11 +13,15 @@ port.pipe(parser)
 var sender = SerialportSender({port: port, parser: new ESP3Parser({ maxBufferSize: 2000 })})
 var commander = CommonCommand.connect(sender)
 
-parser.on("data",pretty.logESP3)
+//parser.on("data",pretty.logESP3)
 async function main () {
-  
+  function col (num) {
+    return `\x1b[38;5;${num}m`
+  }  
   //console.log(await commander.setBaseId(0xffaaaa00))
-  console.log(await commander.getBaseId())
+  // console.log(await commander.getBaseId())
+  // console.log(await commander.addRadioFilter("rorg","block",0xf6))
+  // console.log(await commander.disableFilters())
   // console.log(await commander.setRepeater(0))
   // console.log(await commander.getRepeater())
   //console.log(await commander.addRepeatFilter ("rssi","apply",30))
@@ -27,18 +31,19 @@ async function main () {
   //console.log(await commander.disableWaitMaturity())
   //console.log(await commander.enableSubTel())
   //console.log(await commander.sleep(100))
-  // var addr = await commander.getMemoryAddress(0)
-  // var memory = await commander.readMemory(addr.type,addr.address,addr.length)
-  // process.stdout.write(` ${col(202)}0000\x1b[0m  `)
-  // for(var i = 0;i<memory.data.length;i++){
-  //   process.stdout.write(memory.data[i].toString(16).padStart(2,"0"))
-  //   if(i%4 === 3) process.stdout.write(" ")
-  //   if(i%32 === 31 && i!==memory.data.length-1) process.stdout.write(`\n ${col(202)}${i.toString(16).padStart(4,"0")}\x1b[0m  `)
-  // }
-  // process.stdout.write("\n")
-  // var ex = await commander.readMemory(4,addr.address+63,4)
-  // console.log(ex.data.toString("hex"))
-  // console.log(await commander.writeMemory(4,addr.address+63,"ff830000"))
+   var addr = await commander.getMemoryAddress(0)
+   console.log(addr.type,addr.address,addr.length)
+   var memory = await commander.readMemory(addr.type,addr.address,addr.length)
+   process.stdout.write(` ${col(202)}0000\x1b[0m  `)
+  for(var i = 0;i<memory.data.length;i++){
+    process.stdout.write(memory.data[i].toString(16).padStart(2,"0"))
+    if(i%4 === 3) process.stdout.write(" ")
+    if(i%32 === 31 && i!==memory.data.length-1) process.stdout.write(`\n ${col(202)}${i.toString(16).padStart(4,"0")}\x1b[0m  `)
+  }
+  process.stdout.write("\n")
+  var ex = await commander.readMemory(addr.type,addr.address,4)
+  console.log(ex.data.toString("hex"))
+  console.log(await commander.writeMemory(addr.type,addr.address,"7affffff"))
   // console.log(await commander.getBaseId())
 }
 main()
---
+++ packages/node_modules/@enocean-js/common-command/src/common-commands/CO_SET_BAUDRATE.js
@@ -0,0 +1,5 @@
+export const CO_SET_BAUDRATE = {
+  name: 'CO_SET_BAUDRATE',
+  commandCode: 0x24,
+  responsDefinition: {}
+}
