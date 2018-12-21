b6da9d846235f4cf97bc6a179bfb6e2406b73fbb
Holger Will <h.will@klimapartner.de>
2018-12-21 17:16:23 +0100

    installable sub packages

--- packages/main.js
+++ packages/main.js
@@ -1,3 +1,2 @@
 export * from './enocean.js'
-export { ESP3Parser } from './node_modules/@enocean-js/serialport-parser/src/serialport-parser.js'
-export { ESP3CallbackParser } from './node_modules/@enocean-js/serialport-parser/src/callback-parser.js'
+export * from './node_modules/@enocean-js/serialport-parser/src/serialport-parser.js'
