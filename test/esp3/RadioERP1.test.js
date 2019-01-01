e945367c2253f48c2f2dc3cf823cb8f300767c08
Holger Will <h.will@klimapartner.de>
2019-01-01 10:56:56 +0100

    ?

--- test/esp3/RadioERP1.test.js
+++ test/esp3/RadioERP1.test.js
@@ -189,13 +189,13 @@ describe('RadioERP1 packets', () => {
     })
     it('MSB-LSB splited fields', () => {
       radio = RadioERP1.from({ payload: [0x00, 0x00, 0x00, 0x08] })
-      radio.payload = radio.encode({ LOT: 90, LAT: 0 }, { eep: 'a5-13-06'})
+      radio.payload = radio.encode({ LOT: 90, LAT: 0 }, { eep: 'a5-13-06' })
       decoded = radio.decode('a5-13-06')
       assert.equal(decoded['LOT'].value.toFixed(0), 90)
       assert.equal(decoded['LAT'].value.toFixed(0), 0)
 
       radio = RadioERP1.from({ payload: [0x00, 0x00, 0x00, 0x08] })
-      radio.payload = radio.encode({ SRA: 1900 }, { eep: 'a5-13-10'})
+      radio.payload = radio.encode({ SRA: 1900 }, { eep: 'a5-13-10' })
       decoded = radio.decode('a5-13-10')
       assert.equal(decoded['SRA'].value, 1900)
     })
