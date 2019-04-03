f1d4cf1e7c676a9aaf791d8abebe040ab3b0b6e4
Holger Will <h.will@klimapartner.de>
2019-04-03 12:40:06 +0200

    config node + receiver

--- packages/node-red-contrib-enocean/enocean-node.js
+++ packages/node-red-contrib-enocean/enocean-node.js
@@ -3,19 +3,62 @@ const SerialPort = require('serialport')
 const ESP3Transfomer = require("@enocean-js/esp3-packets").ESP3Transformer
 
 module.exports = function(RED) {
-    function EnoceanNode(config) {
-        RED.nodes.createNode(this,config);
-        const port = new SerialPort('/dev/ttyUSB0', { baudRate: 57600 })
-        const transformer = new ESP3Transfomer()
-        const parser = new ESP3Parser()
-        var node = this;
-        port.pipe(parser).pipe(transformer)
-        transformer.on('data', data=>{
-          node.send({payload:data})
-          node.log(data)
-          node.log(node.bla)
-          node.bla = "123"
-        })
-    }
-    RED.nodes.registerType("enocean",EnoceanNode);
+  function EnOceanConfigNode(config) {
+      RED.nodes.createNode(this,config);
+      this.serialport = config.serialport;
+      this.port = null
+      try{
+        console.log("hallo")
+        this.port = new SerialPort(this.serialport, { baudRate: 57600 })
+      }catch(err){
+        console.log(err)
+      }
+  }
+  RED.nodes.registerType("enocean-config-node",EnOceanConfigNode);
+
+  function EnoceanActorNode(config) {
+    RED.nodes.createNode(this,config);
+    this.eep = config.eep
+    this.senderId = config.senderid
+    var node=this
+    EnoceanListener(node,config,data=>{
+      if(data.senderId===node.senderId){
+        node.send({payload: data.decode(node.eep) })
+      }
+      if(node.senderId===""){
+        console.log("unknown",data.teachIn)
+        if(data.teachIn){
+          console.log(data.teachInInfo)
+          node.send({
+            payload: {
+              senderId:data.teachInInfo.senderId,
+              eep:data.teachInInfo.eep.toString(),
+            }
+          })
+        }
+      }
+    })
+  }
+  RED.nodes.registerType("enocean-actor",EnoceanActorNode);
+
+  function EnoceanListener(node,config,cb){
+    const transformer = new ESP3Transfomer()
+    const parser = new ESP3Parser()
+    const usb = RED.nodes.getNode(config.serialport)
+    node.on('close', function (done) {
+      usb.port.close(done);
+    });
+    usb.port.pipe(parser).pipe(transformer)
+    transformer.on('data', cb)
+  }
+
+  // function EnoceanTeachInInfoNode(config) {
+  //   RED.nodes.createNode(this,config);
+  //   EnoceanListener(this,config,data=>{
+  //     if(data.teachIn){
+  //       node.send({payload: data.teachInInfo })
+  //     }
+  //   })
+  // }
+  // RED.nodes.registerType("enocean-teach-in-info", EnoceanTeachInInfoNode);
 }
