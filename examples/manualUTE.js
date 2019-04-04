1032e453d8d8d23f2354741424e7817b9b614240
Andreas <andiglueck@gmail.com>
2019-04-04 22:04:38 +0200

    fix linting

--- examples/manualUTE.js
+++ examples/manualUTE.js
@@ -9,69 +9,66 @@ const parser = new ESP3Parser()
 const transformer = new ESP3Transformer()
 const sender = Enocean.SerialportSender({ port: port, parser: new ESP3Parser() })
 const Commander = new Enocean.Commander(sender)
-var baseId = ""
+var baseId = ''
 
 // add timestamp to the console logging
-console.logCopy = console.log.bind(console);
-console.log = function(data)
-{
-    var currentDate = '[' + new Date().toISOString().replace(/z|t/gi,' ').trim() + '] ';
-    this.logCopy(currentDate, data);
-};
+console.logCopy = console.log.bind(console)
+console.log = function (data) {
+  var currentDate = '[' + new Date().toISOString().replace(/z|t/gi, ' ').trim() + '] '
+  this.logCopy(currentDate, data)
+}
 
-console.log("--- start ---")
+console.log('--- start ---')
 
 port.pipe(parser).pipe(transformer)
 
-async function init() {
-    var res = await Commander.getIdBase()
-    baseId = parseInt(res.baseId.toString(), 16)
-    console.log("BaseId:" + res.baseId.toString())
+async function init () {
+  var res = await Commander.getIdBase()
+  baseId = parseInt(res.baseId.toString(), 16)
+  console.log('BaseId:' + res.baseId.toString())
 }
 
 // array to store the teached in deviced
 var known = {}
 // add the previously teached devices manually
 // (there is no storage of teached devices yet)
-known["050e0ed1"] = {eep:'d2-50-00'}
+known['050e0ed1'] = { eep: 'd2-50-00' }
 
 // register on new data received
 transformer.on('data', async data => {
-    console.log("--- new Data! --- ")
-    if (data && data.constructor.name === "RadioERP1") {
-        console.log("ERP1")
-        console.log("Data: " + data.toString())
-        if (data.teachIn) {
-            console.log("TeachIn")
-            var teachInInfo = data.teachInInfo
-            console.log("TeachIn telegram")
-            if(!known.hasOwnProperty(teachInInfo.senderId)){
-                if(baseId==="") await init()
-                known[data.senderId] = teachInInfo
-                //*****************************************************************************
-                var ret = RadioERP1.from({rorg: 0xd4,payload:data.payload})
-                ret.senderId = baseId + 1
-                ret.destinationId = data.senderId // the response needs to be sent directy to the device (no broadcast)
-                // setValue (value, bitoffset, length)
-                ret.payload = ret.payload.setValue(1,0,1) // bidi
-                ret.payload = ret.payload.setValue(1,2,2) // teach in successful
-                ret.payload = ret.payload.setValue(1,4,4) // this is a teach in response
-                console.log("Response: " + ret.toString())
-                console.log("Sent: " + await sender.send(ret.toString()))
-                //*******************************************************************************
-            }
-            else {
-                console.log("TeachIn from already known sender")
-            }
-        } else {
-            console.log("Data telegram")
-            if (known.hasOwnProperty(data.senderId)) {
-                console.log("Known sender: " + data.senderId)
-                console.log(data.decode(known[data.senderId].eep.toString()))
-            }
-        }
+  console.log('--- new Data! --- ')
+  if (data && data.constructor.name === 'RadioERP1') {
+    console.log('ERP1')
+    console.log('Data: ' + data.toString())
+    if (data.teachIn) {
+      console.log('TeachIn')
+      var teachInInfo = data.teachInInfo
+      console.log('TeachIn telegram')
+      if (!known.hasOwnProperty(teachInInfo.senderId)) {
+        if (baseId === '') await init()
+        known[data.senderId] = teachInInfo
+        //* ****************************************************************************
+        var ret = RadioERP1.from({ rorg: 0xd4, payload: data.payload })
+        ret.senderId = baseId + 1
+        ret.destinationId = data.senderId // the response needs to be sent directy to the device (no broadcast)
+        // setValue (value, bitoffset, length)
+        ret.payload = ret.payload.setValue(1, 0, 1) // bidi
+        ret.payload = ret.payload.setValue(1, 2, 2) // teach in successful
+        ret.payload = ret.payload.setValue(1, 4, 4) // this is a teach in response
+        console.log('Response: ' + ret.toString())
+        console.log('Sent: ' + await sender.send(ret.toString()))
+        //* ******************************************************************************
+      } else {
+        console.log('TeachIn from already known sender')
+      }
+    } else {
+      console.log('Data telegram')
+      if (known.hasOwnProperty(data.senderId)) {
+        console.log('Known sender: ' + data.senderId)
+        console.log(data.decode(known[data.senderId].eep.toString()))
+      }
     }
-    else {
-        console.log("Non RadioERP1 data: " + data.toString())
-    }
+  } else {
+    console.log('Non RadioERP1 data: ' + data.toString())
+  }
 })
