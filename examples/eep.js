d056bc9da2aca21823a68ac65ab6d7ea4d792869
Holger Will <h.will@klimapartner.de>
2019-04-10 06:00:22 +0200

    wip

--- examples/eep.js
+++ examples/eep.js
@@ -14,7 +14,6 @@ var baseId=""
 
 port.pipe(parser).pipe(transformer)
 
-
 async function init(){
   var res = await Commander.getIdBase()
   baseId = parseInt(res.baseId.toString(), 16)
@@ -23,34 +22,29 @@ async function init(){
 var known = {}
 const ESP3Packet = Enocean.ESP3Packet
 transformer.on('data', async data => {
-  console.log(data.decode("d2-50-00"))
+  //console.log(data.decode("d2-50-00"))
   if (data && data.constructor.name === "RadioERP1") {
     if(data.teachIn){
       var teachInInfo = data.teachInInfo
+      console.log(teachInInfo,data.RSSI)
       if(!known.hasOwnProperty(teachInInfo.senderId)){
         if(baseId==="") await init()
         known[data.senderId] = teachInInfo
         radio = RadioERP1.from({ payload: [0], id: 'ff00ff00' })
         radio.payload = radio.encode({ MT: 0, RMT: 1 }, { eep: 'd2-50-00', data: 0})
         radio.senderId = 'ff00ff00'
-        
         decoded = radio.decode('d2-50-00')
-        //*****************************************************************************
-        var ret = RadioERP1.from({rorg: 0xd4,payload:data.payload})
-        ret.senderId = baseId + 1
-        ret.destinationId = data.senderId
-        ret.payload = ret.payload.setValue(1,0,1) // bidi
-        ret.payload = ret.payload.setValue(1,2,2) // teach in successful
-        ret.payload = ret.payload.setValue(1,4,4) // this is a teach in response
-        console.log(ret)
-        console.log(RadioERP1.makeTeachIn({
-          rorg: 0xd4,
+
+        var ret = RadioERP1.makeTeachIn({
+          type: 'UTE',
           requestPayload: data.payload,
           bidi: Enocean.UTE_BIDIRECTIONAL,
           result: Enocean.UTE_TEACH_IN_SUCCESSFULL,
-          cmd: Enocean.UTE_CMD_RESPONSE
-        }))
-        console.log(await sender.send(ret.toString()))
+          cmd: Enocean.UTE_CMD_RESPONSE,
+          destinationId: data.senderId,
+          senderId: baseId + 1
+        })
+        //console.log(await sender.send(ret.toString()))
         //*******************************************************************************
       }
     }else{
@@ -60,4 +54,16 @@ transformer.on('data', async data => {
     }
   }
 })
-transformer.write(ESP3Packet.from("550014070165d24103003d00935000003c0f21c21c050e0d480001ffffffff4400a0"))
+
+var ret = RadioERP1.makeTeachIn({
+  type: '4BS',
+  eep: "a5-04-01",
+  senderId: 0x11223344//baseId + 1
+})
+console.log(ret.teachInInfo)
+radio = RadioERP1.from({ payload: [0x00, 0x00, 0x00, 0x08] })
+radio.payload = radio.encode({HUM:50,TMP:22,TSN:1},{eep:"a5-04-01"})
+radio.senderId = 0x11223344
+console.log(radio.toString())
+//transformer.write(ESP3Packet.from("55000d0701fdd480ff61000050d2050e0ed10001ffffffff34007b"))
+//transformer.write(ESP3Packet.from("550014070165d24103003d00935000003c0f21c21c050e0d480001ffffffff4400a0"))
