7f8ef5488fe03651cedd95b3e51b0cb07df89f47
Holger Will <h.will@klimapartner.de>
2019-04-13 06:17:06 +0200

    advanced eep selction

--- packages/node-red-contrib-enocean/enocean-node.js
+++ packages/node-red-contrib-enocean/enocean-node.js
@@ -19,7 +19,6 @@ module.exports = function (RED) {
       this.getBaseId = async function () {
         var res = await this.commander.getIdBase()
         node.baseId = parseInt(res.baseId.toString(), 16)
-        console.log(res)
       }
       this.getBaseId()
     } catch (err) {
@@ -32,6 +31,8 @@ module.exports = function (RED) {
     RED.nodes.createNode(this, config)
     this.eep = config.eep
     this.offset = config.offset
+    this.direction = config.direction
+    this.data = config.data
     this.serialport = RED.nodes.getNode(config.serialport)
     var node = this
     node.on('input', async function (msg) {
@@ -41,7 +42,7 @@ module.exports = function (RED) {
           await node.serialport.sender.send(te.toString())
         }
       } else {
-        var tel = RadioERP1.from({ eep: node.eep, payload: [0, 0, 0, 0], id: this.serialport.baseId + parseInt(node.offset) })
+        var tel = RadioERP1.from({ eep: node.eep, payload: [0, 0, 0, 0], id: this.serialport.baseId + parseInt(node.offset), direction: node.direction, data: node.data })
         tel.payload = tel.encode(msg.payload, { eep: node.eep })
         await node.serialport.sender.send(tel.toString())
       }
@@ -57,15 +58,12 @@ module.exports = function (RED) {
 
     node.on('input', async function (msg) {
       node.btn = RadioERP1.from({ eep: 'f6-02-01', payload: [0], id: node.serialport.baseId + parseInt(node.offset) })
-      console.log()
       async function release () {
         node.btn.payload = node.btn.encode({ R1: 0, EB: 0 }, { eep: 'f6-02-01', status: 0x20 })
-        console.log(node.btn.toString())
         await node.serialport.sender.send(node.btn.toString())
       }
       async function btnDown (btn) {
         node.btn.payload = node.btn.encode({ R1: btn, EB: 1 }, { eep: 'f6-02-01', status: 0x30 })
-        console.log(node.btn.toString())
         await node.serialport.sender.send(node.btn.toString())
       }
       if (msg.payload === 'A0_down') {
@@ -107,6 +105,7 @@ module.exports = function (RED) {
     RED.nodes.createNode(this, config)
     this.eep = config.eep
     this.senderId = config.senderid
+    this.direction = config.direction
     var node = this
     if (node.senderId === '' || node.eep === '') {
       this.status({ fill: 'red', shape: 'ring', text: 'unknown EEP' })
@@ -118,7 +117,7 @@ module.exports = function (RED) {
         if (data.RORG !== 0xf6 && data.teachIn) return
         if (data.RORG.toString(16) !== node.eep.split('-')[0]) return
         node.send({
-          payload: data.decode(node.eep)
+          payload: data.decode(node.eep, node.direction)
         })
       }
       if (node.senderId === '' || node.eep === '') {
