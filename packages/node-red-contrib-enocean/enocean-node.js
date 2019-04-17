5178bd89f26750fc4ed5ef9c5326fdbf012daf58
Holger Will <h.will@klimapartner.de>
2019-04-17 06:49:32 +0200

    lint

--- packages/node-red-contrib-enocean/enocean-node.js
+++ packages/node-red-contrib-enocean/enocean-node.js
@@ -52,9 +52,9 @@ module.exports = function (RED) {
         if (msg.payload === 'LRN') {
           var te = RadioERP1.makeTeachIn({ eep: node.eep, senderId: this.serialport.baseId + parseInt(node.offset) })
           await node.serialport.sender.send(te.toString())
-          var tel = RadioERP1.from({ eep: node.eep, payload: [0, 0, 0, 0], id: this.serialport.baseId + parseInt(node.offset), direction: node.direction, data: node.data })
-          tel.payload = tel.encode({}, { eep: node.eep, direction: node.direction, data: node.data })
-          await node.serialport.sender.send(tel.toString())
+          var tel0 = RadioERP1.from({ eep: node.eep, payload: [0, 0, 0, 0], id: this.serialport.baseId + parseInt(node.offset), direction: node.direction, data: node.data })
+          tel0.payload = tel0.encode({}, { eep: node.eep, direction: node.direction, data: node.data })
+          await node.serialport.sender.send(tel0.toString())
         }
       } else {
         var tel = RadioERP1.from({ eep: node.eep, payload: [0, 0, 0, 0], id: this.serialport.baseId + parseInt(node.offset), direction: node.direction, data: node.data })
