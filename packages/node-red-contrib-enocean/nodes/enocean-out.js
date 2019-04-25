e42d23fbe4129efce990b61c6d449b86ab5817f8
Holger Will <h.will@klimapartner.de>
2019-04-25 06:39:49 +0200

    [NRCE] generalize enocean-out

--- packages/node-red-contrib-enocean/nodes/enocean-out.js
+++ packages/node-red-contrib-enocean/nodes/enocean-out.js
@@ -9,22 +9,40 @@ module.exports = RED => {
     this.serialport = RED.nodes.getNode(config.serialport)
     var node = this
     node.on('input', async function (msg) {
-      if (typeof msg.payload === 'string') {
-        if (msg.payload === 'LRN') {
-          var te = RadioERP1.makeTeachIn({ eep: node.eep, senderId: this.serialport.baseId + parseInt(node.offset) })
-          await node.serialport.sender.send(te.toString())
-          var tel0 = RadioERP1.from({ eep: node.eep, payload: [0, 0, 0, 8], id: this.serialport.baseId + parseInt(node.offset), direction: node.direction, data: node.data })
-          tel0.teachIn = false
-          tel0.payload = tel0.encode({}, { eep: node.eep, direction: node.direction, data: node.data })
-          await node.serialport.sender.send(tel0.toString())
-        }
+      var senderId = this.serialport.baseId + parseInt(msg.payload.meta.channel)
+      if (msg.payload.meta.type === 'teach-in') {
+        // send teach-in
+        var te = RadioERP1.makeTeachIn({ type: msg.payload['teach-in'].type, eep: msg.payload.meta.eep, senderId: senderId })
+        await node.serialport.sender.send(te.toString())
+        // followed by a data telegram
+        var tel0 = RadioERP1.from({ eep: msg.payload.meta.eep, payload: [0, 0, 0, 8], id: senderId, direction: msg.payload.meta.direction || 1, data: msg.payload.meta.data || 0, status: msg.payload.meta.status || 0 })
+        tel0.teachIn = false
+        tel0.payload = tel0.encode(msg.payload.data, { eep: msg.payload.meta.eep, direction: msg.payload.meta.direction || 1, data: msg.payload.meta.data || 0, status: msg.payload.meta.status || 0 })
+        await node.serialport.sender.send(tel0.toString())
       } else {
-        var tel = RadioERP1.from({ eep: node.eep, payload: [0, 0, 0, 8], id: this.serialport.baseId + parseInt(node.offset), direction: node.direction, data: node.data })
+        var tel = RadioERP1.from({ eep: msg.payload.meta.eep, payload: [0, 0, 0, 8], id: senderId, direction: msg.payload.meta.direction || 1, data: msg.payload.meta.data || 0, status: msg.payload.meta.status || 0 })
         tel.teachIn = false
-        tel.payload = tel.encode(msg.payload, { eep: node.eep, direction: node.direction, data: node.data })
+        tel.payload = tel.encode(msg.payload.data, { eep: msg.payload.meta.eep, direction: msg.payload.meta.direction || 1, data: msg.payload.meta.data || 0, status: msg.payload.meta.status || 0 })
         await node.serialport.sender.send(tel.toString())
       }
     })
   }
   return EnOceanOutputNode
 }
+//
+// {
+//   "meta": {
+//     "eep": "a5-12-04",
+//     "channel": 3
+//     "type": "data", // teach-in
+//     "direction": 1,
+//     "data": 0,
+//     "status": 0
+//   },
+//   "teach-in":{
+//     "type": "4BS"
+//   },
+//   "data": {
+//     "TMP": 21
+//   }
+// }
