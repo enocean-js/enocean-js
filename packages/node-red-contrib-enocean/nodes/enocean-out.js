d8f6ea4e9a2b860a3b629938e72ae8ddcd45255d
Holger Will <h.will@klimapartner.de>
2019-05-14 11:12:31 +0200

    lint

--- packages/node-red-contrib-enocean/nodes/enocean-out.js
+++ packages/node-red-contrib-enocean/nodes/enocean-out.js
@@ -7,39 +7,38 @@ module.exports = RED => {
     this.serialport = RED.nodes.getNode(config.serialport)
     var node = this
     node.on('input', async function (msg) {
-      if(typeof msg.payload === "string"){
-        await enoSend(node,msg.payload)
+      if (typeof msg.payload === 'string') {
+        await enoSend(node, msg.payload)
         return
       }
       var senderId = this.serialport.baseId + parseInt(msg.payload.meta.channel)
       if (msg.payload.meta.type === 'teach-in') {
         // send teach-in
         var te = RadioERP1.makeTeachIn({ type: msg.payload['teach-in'].type, eep: msg.payload.meta.eep, senderId: senderId })
-        await enoSend(node,te.toString())
+        await enoSend(node, te.toString())
         // followed by a data telegram
         var tel0 = RadioERP1.from({ rorg: parseInt(msg.payload.meta.eep.split('-')[0], 16), eep: msg.payload.meta.eep, payload: [0], id: senderId, direction: msg.payload.meta.direction || 1, data: msg.payload.meta.data || 0, status: msg.payload.meta.status || 0 })
         tel0.teachIn = false
         tel0.encode(msg.payload.data, { eep: msg.payload.meta.eep, direction: msg.payload.meta.direction || 1, data: msg.payload.meta.data || 0, status: msg.payload.meta.status || 0 })
-        await enoSend(node,tel0.toString())
+        await enoSend(node, tel0.toString())
       } else {
         var tel = RadioERP1.from({ rorg: parseInt(msg.payload.meta.eep.split('-')[0], 16), eep: msg.payload.meta.eep, payload: [0], id: senderId, direction: msg.payload.meta.direction || 1, data: msg.payload.meta.data || 0, status: msg.payload.meta.status || 0 })
         tel.encode(msg.payload.data, { eep: msg.payload.meta.eep, direction: msg.payload.meta.direction || 1, data: msg.payload.meta.data || 0, status: msg.payload.meta.status || 0 })
         tel.teachIn = false
-        await enoSend(node,tel.toString())
+        await enoSend(node, tel.toString())
       }
     })
   }
   RED.nodes.registerType('enocean-out', EnOceanOutputNode)
 }
 
-async function enoSend(node,data){
+async function enoSend (node, data) {
   var ret = await node.serialport.sender.send(data)
   var res = Response.from(ret)
   if (res.responseType !== 'RET_OK') {
     node.warn(res.responseType)
   }
   if (node.logOutput) {
-
     node.send({
       payload: data,
       return: res
