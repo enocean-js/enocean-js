3a4f186af0afaa1e464fbda806319fa4aab7d628
Holger Will <h.will@klimapartner.de>
2019-05-16 19:42:30 +0200

    lint

--- packages/node-red-contrib-enocean/nodes/enocean-out.js
+++ packages/node-red-contrib-enocean/nodes/enocean-out.js
@@ -30,7 +30,7 @@ module.exports = RED => {
         await enoSend(node, te.toString())
         // followed by a data telegram
         var tel0 = RadioERP1.from({ rorg: parseInt(msg.payload.meta.eep.split('-')[0], 16), eep: msg.payload.meta.eep, payload: [0], id: senderId, direction: msg.payload.meta.direction || 1, data: msg.payload.meta.data || 0, status: msg.payload.meta.status || 0 })
-        if(msg.payload.meta.eep.split("-")[0]!=="f6"){
+        if (msg.payload.meta.eep.split('-')[0] !== 'f6') {
           tel0.teachIn = false
         }
         tel0.encode(msg.payload.data, { eep: msg.payload.meta.eep, direction: msg.payload.meta.direction || 1, data: msg.payload.meta.data || 0, status: msg.payload.meta.status || 0 })
@@ -38,7 +38,7 @@ module.exports = RED => {
       } else {
         var tel = RadioERP1.from({ rorg: parseInt(msg.payload.meta.eep.split('-')[0], 16), eep: msg.payload.meta.eep, payload: [0], id: senderId, direction: msg.payload.meta.direction || 1, data: msg.payload.meta.data || 0, status: msg.payload.meta.status || 0 })
         tel.encode(msg.payload.data, { eep: msg.payload.meta.eep, direction: msg.payload.meta.direction || 1, data: msg.payload.meta.data || 0, status: msg.payload.meta.status || 0 })
-        if(msg.payload.meta.eep.split("-")[0]!=="f6"){
+        if (msg.payload.meta.eep.split('-')[0] !== 'f6') {
           tel0.teachIn = false
         }
         await enoSend(node, tel.toString())
