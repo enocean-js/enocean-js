13a515435808f31c5866359859e3d59c9ff7792e
Holger Will <h.will@klimapartner.de>
2020-02-25 08:13:35 +0100

    remove unneeded await

--- examples/elements/web-serial-connector.js
+++ examples/elements/web-serial-connector.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef  */
 import { html, css, LitElement } from 'https://cdn.klimapartner.net/modules/lit-element/lit-element.js'
 import { until } from 'https://cdn.klimapartner.net/modules/lit-html/directives/until.js'
-import {ESP3WebSerialParser} from '../../packages/enocean.js'
+import { ESP3WebSerialParser } from '../../packages/enocean.js'
 export class WebSerialConnector extends LitElement {
   constructor () {
     super()
@@ -12,22 +12,25 @@ export class WebSerialConnector extends LitElement {
       console.log('WebSerial not supported by this browser')
     }
   }
+
   getPorts () {
     this.ports = navigator.serial.getPorts().then(async pl => {
       if (pl.length === 0) {
         return html`<button id="btn_addport" @click="${this.requestPort}">+</button>`
       } else {
         this.port = pl[this.portNumber]
-        return await this.open()
+        return this.open()
       }
     })
   }
+
   async requestPort () {
     this.port = await navigator.serial.requestPort()
     await this.open()
   }
+
   async open () {
-    await this.port.open({baudrate: 57600})
+    await this.port.open({ baudrate: 57600 })
     this.ports = html`<button class="open" @click="${this.close}">close</button>`
 
     this.parser = new ESP3WebSerialParser()
@@ -37,22 +40,26 @@ export class WebSerialConnector extends LitElement {
     this.writer.write(new Uint8Array([85, 0, 1, 0, 5, 112, 8, 56]))
     return this.ports
   }
+
   async close () {
     await this.port.close()
     console.log('closing')
     this.ports = html`<button class="closed" @click="${this.open}">open</button>`
   }
+
   static get styles () {
     return css`
       #btn_addport{background:green}
     `
   }
+
   static get properties () {
     return {
-      port: {type: Object},
-      ports: {type: Object}
+      port: { type: Object },
+      ports: { type: Object }
     }
   }
+
   render () {
     return html`
       <div>${until(this.ports, 'loading...')}</div>
