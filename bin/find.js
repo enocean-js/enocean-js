6b4e349f13fa44ed221d128e3be1d28fd7312e1f
Holger Will <h.will@klimapartner.de>
2020-12-15 07:46:02 +0100

    standardize

--- bin/find.js
+++ bin/find.js
@@ -1,25 +1,25 @@
-const ENOCEAN = require("../")
+const ENOCEAN = require('../')
 const EEP = ENOCEAN.EEP
-for(eep in EEP){
-  search(EEP[eep],eep)
+for (const eep in EEP) {
+  search(EEP[eep], eep)
 }
 
-function search(obj,eep){
-  for(item in obj){
-    if(item === "shortcut"){
-      if(typeof obj.shortcut === "string"){
-        if(hasLowerCase(obj.shortcut)){
-          console.log(eep,obj.shortcut)
+function search (obj, eep) {
+  for (const item in obj) {
+    if (item === 'shortcut') {
+      if (typeof obj.shortcut === 'string') {
+        if (hasLowerCase(obj.shortcut)) {
+          console.log(eep, obj.shortcut)
         }
       }
     } else {
-      if(typeof obj[item] === "object"){
-        search(obj[item],eep)
+      if (typeof obj[item] === 'object') {
+        search(obj[item], eep)
       }
     }
   }
 }
 
-function hasLowerCase(str) {
-    return str.toUpperCase() != str;
+function hasLowerCase (str) {
+  return str.toUpperCase() !== str
 }
